afa8ad2d35eec208759877a09d48853f89b61bd0
danonin <daniel.nahrebecki@gmail.com>
2013-05-21 22:30:23 +0200

    html na php w widokach content
    rozciągnąłem layout na 100%, bo z tym 75% to tak trochę dziwnie
    wyglądało

--- Radioziwg/application/views/wrapper.php
+++ Radioziwg/application/views/wrapper.php
@@ -3,24 +3,24 @@
 header('Content-Type: text/html; charset=utf-8');
         /*tutaj pasek nawigacyjny musi być ładowany czy jest ktoś zalogowany czy też czy jest to użytkownik czy admin*/
         $this->load->view('header');
-        
+
         //$this->load->view('navigation/foradmin');
         if ($this->session->userdata('id') !== FALSE) {
-           if ($this->session->userdata('isAdmin') !== FALSE) {
-               $this->load->view('navigation/foradmin');
-           }else{
-               $this->load->view('navigation/forusers');
-           }
-           }else{
-        $this->load->view('navigation/foreveryone');
+            if ($this->session->userdata('isAdmin') !== FALSE) {
+                $this->load->view('navigation/foradmin');
+            }else{
+                $this->load->view('navigation/forusers');
+            }
+        } else{
+            $this->load->view('navigation/foreveryone');
         }
         //$this->load->view('navigation/foreveryone');
 
-       
+
        $this->load->view('radios/radios');
-        
-		/* tutaj musi być ładowane radio w zależności od kliknięcia na grafikę radia */
-       
+
+                /* tutaj musi być ładowane radio w zależności od kliknięcia na grafikę radia */
+
        if($radio=='classic'){
          $this->load->view('radios/classicradio');  
        }elseif($radio=='rock'){
@@ -31,17 +31,13 @@ header('Content-Type: text/html; charset=utf-8');
            $this->load->view('radios/metalradio'); 
        }
        
-       // 
-       // 
-      if ($this->session->userdata('id') !== FALSE) {
-   
-         $this->load->view('login/logout');
+        if ($this->session->userdata('id') !== FALSE) {
+            $this->load->view('login/logout');
         } else {
-        $this->load->view('login/login');
+            $this->load->view('login/login');
         }
-        
+
         $this->load->view('content/'.$content);
-       // $this->load->view('body');
-                
+
         $this->load->view('footer');
 ?>
