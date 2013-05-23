a682994231716276be1cf276d3837ed7af607742
danonin <daniel.nahrebecki@gmail.com>
2013-05-23 10:00:53 +0200

    poprawki kodowanie

--- Radioziwg/application/views/content/mainpage.php
+++ Radioziwg/application/views/content/mainpage.php
@@ -4,32 +4,32 @@
                         <div class="span5 span5-ext bgblue2">
                             <div>
                                 <article>
-                                    <p class="nagl">Aktualnie s�uchane stacje:</p>
+                                    <p class="nagl">Aktualnie słuchane stacje:</p>
                                     <table>
                                         <tr class="tlo">
                                             <td class="logo"><img src="images/metal.png" width="40px"/></td>
                                             <td class="nazwaradia"><p> Radio Metal</p></td>
                                             <td class="play"><i class="icon-play icon-white"></i></td>
-                                            <td class="utwor"><p><em>Tytu� utworu </em>- Wykonawca</p></td>
+                                            <td class="utwor"><p><em>Tytuł utworu </em>- Wykonawca</p></td>
                                         </tr>
                                         <tr >
                                             <td class="logo"><img src="images/rock.png" width="40px"/></td>
                                             <td class="nazwaradia"><p>   Radio Rock  </p></td>
                                             <td class="play"><i class="icon-play icon-white"></i></td>
-                                            <td class="utwor"><p><em>Tytu� utworu </em>- Wykonawca (D�u�sza nazwa)</p>
+                                            <td class="utwor"><p><em>Tytuł utworu </em>- Wykonawca (Dłuższa nazwa)</p>
                                             </td>
                                         </tr>
                                         <tr class="tlo">
                                             <td class="logo"><img src="images/classic.png" width="40px"/></td>
                                             <td class="nazwaradia"><p> Radio Classic</p></td>
                                             <td class="play"><i class="icon-play icon-white"></i></td>
-                                            <td class="utwor"><p><em>Tytu� utworu/piosenki </em>- Wykonawca</p></td>
+                                            <td class="utwor"><p><em>Tytuł utworu/piosenki </em>- Wykonawca</p></td>
                                         </tr>
                                         <tr class="tlo">
                                             <td class="logo"><img src="images/metal.png" width="40px"/></td>
                                             <td class="nazwaradia"><p> RMF FM</p></td>
                                             <td class="play"><i class="icon-play icon-white"></i></td>
-                                            <td class="utwor"><p><em>Tytu� utworu </em>- Wykonawca</p></td>
+                                            <td class="utwor"><p><em>Tytuł utworu </em>- Wykonawca</p></td>
                                         </tr>
                                     </table>
                                 </article>
@@ -37,7 +37,7 @@
                         </div>
                         <div class="span3 span3-ext bgblue2">
                             <p class="nagl">Gitarowy Rekord Guinnessa na 5734 gitar!</p>
-                            <p>A� 5734 gitarzyst�w przyby�o 1 maja na wroc�awski Rynek, by zagra� w wielkiej orkiestrze, kt�r� dyrygowa� Leszek Cicho�ski. �Hey Joe� Jimiego Hendrixa wsp�lnie z tysi�cami gitarzyst�w zagra�a tak�e Jennifer Batten - gitarzystka Michaela Jacksona, Bruce Kulick � gitarzysta KISS, John Corabi - wokalista i gitarzysta M�tley Cr�e oraz Chuck Garric (gitarzysta Alice Coopera).</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus laoreet bibendum turpis, ut euismod risus lacinia tincidunt. Praesent varius ornare lacus, non consectetur nisl pharetra sit amet. Pellentesque cursus nibh eget urna lacinia tincidunt. Aenean eget urna sed dui mattis vulputate ut sed ligula. Sed turpis mi, euismod a rhoncus eu, venenatis ac risus. Donec ac urna lorem. Donec ultrices imperdiet ultricies. Morbi in mauris purus, et sodales tortor. Fusce nec mattis massa. Vivamus aliquet venenatis sem non lacinia. Nulla tincidunt consequat neque, a eleifend dui volutpat non. Nam congue ornare libero in mattis. Aliquam erat volutpat.</p>
                             <a href="http://www.rmfclassic.pl/informacje/Muzyka,10/Gitarowy-Rekord-Guinnessa-na-5734-gitar,19793.html">Czytaj dalej...</a>
                         </div>
                 </div>
